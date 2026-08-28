05caf52e77bf0c421e2eaef0fef2b30b59ffb23a
Neo-treee <s.itsuki0712@gmail.com>
2026-08-28 21:47:00 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,32 @@
+const CACHE_NAME = 'toilet-map-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './App.js'
+];
+
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Installed');
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME)
+                  .map((name) => caches.delete(name))
+      );
+    })
+  );
 });
