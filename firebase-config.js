c4e0d3b2cc6c26116fde12e3462a0cc66e00d254
Neo-treee <s.itsuki0712@gmail.com>
2026-08-30 17:37:58 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,9 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyA0ixB0Wj-rSo7X7VOUZZROP8lUZBj6vJg",
+  authDomain: "my-toilet-app-3c2c3.firebaseapp.com",
+  projectId: "my-toilet-app-3c2c3",
+  storageBucket: "my-toilet-app-3c2c3.firebasestorage.app",
+  messagingSenderId: "322835199622",
+  appId: "1:322835199622:web:7e43f787b97a003d408307"
+};
+firebase.initializeApp(firebaseConfig);
